f409331608138035c933cbdfe471af1a01ae28ee
SandroDev <sandrodevx@gmail.com>
2025-10-06 19:24:10 +0200

    front-end ready

--- 01-proyecto-traducciones-openai/public/assets/js/main.js
+++ 01-proyecto-traducciones-openai/public/assets/js/main.js
@@ -1,47 +1,40 @@
-let translateButton = document.querySelector("#translateButton");
+// Seleccionar elementos del dom
+const generateBtn = document.querySelector(".btn-generate");
+const avatarBox = document.querySelector(".avatar-box");
+const loading = document.querySelector(".loading");
+const categorySelector = document.querySelector(".category-selector");
 
-translateButton.addEventListener("click", async () => {
-  let inputText = document.querySelector("#inputText");
-  //valor a traducir
-  const text = inputText.value.trim();
+loading.style.display = "none";
 
-  // lenguaje de destino
-  const targetLang = document.querySelector("#targetLang").value;
-
-  if (!text) return false;
-  // meter el mensaje del usuario a la caja de mensajes
-  const userMessage = document.createElement("div");
-  userMessage.className = "chat__message chat__message--user";
-  userMessage.textContent = text;
-
-  const messagesContainer = document.querySelector(".chat__messages");
-  messagesContainer.appendChild(userMessage);
-  messagesContainer.scrollTop = messagesContainer.scrollHeight;
-  // peticion ajax al backend
+generateBtn.addEventListener("click", async () => {
+  // sacar la categoria seleccionada
+  const category = categorySelector.value;
+  // Mostrar loading
+  loading.style.display = "block";
+  // Peticion ajax al backend
   try {
-    const response = await fetch("/api/traducir", {
+    const response = await fetch("http://localhost:3000/api/gen-img", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ text, targetLang }),
+      body: JSON.stringify({ category }),
     });
 
-    const data = await response.json();
-    //agragere el mensaje de la ia al chat
+    let data = await response.json();
 
-    alert(data.translatedText);
-    const botMessage = document.createElement("div");
-    botMessage.className = "chat__message chat__message--bot";
-    botMessage.textContent = data.translatedText;
-    messagesContainer.appendChild(botMessage);
-    messagesContainer.scrollTop = messagesContainer.scrollHeight;
+    // incrustrar la imagen en la caja de avatar
+    if (data && data.imageUrl) {
+      avatarBox.innerHTML = `<img src="${data.imageUrl}" alt="Avatar" />`;
+    } else {
+      alert("Error al generar la imagen");
+    }
   } catch (error) {
-    console.log("Error:");
+    console.log("Error al generar la imagen:", error);
+    alert("Error al generar la imagen");
+  } finally {
+    // Ocultar loading
+    loading.style.display = "none";
   }
-  //agragere el mensaje de la ia al chat
-
-  //vaciar el input de tipo texto
-
-  inputText.value = "";
+  // incrustrar la imagen en la caja de avatar
 });
